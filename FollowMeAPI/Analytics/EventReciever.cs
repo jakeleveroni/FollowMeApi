537e19bc47199b2b95fa8b79e519d655c213b71c
Jacob <jakeleveroni@yahoo.com>
2017-02-27 20:38:33 -0800

    added the basic outline for how the message based event system will function in our api

---
+++ FollowMeAPI/Analytics/EventReciever.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/*
+ * This class will be responsible for receiving all events fired off by users and
+ * performing the correct actions based on those events 
+ */
+
+namespace FollowMeAPI.Analytics
+{
+    public static class EventReciever
+    {
+    }
+}
---
+++ FollowMeAPI/Controllers/EventController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+/*
+ * This will be the controller we add events to for user badges as well as analytics 
+ * the various routes we define here will fire off events to the EventReceiver which will 
+ * handle behave appropriately based on the event being fired
+ */
+
+namespace FollowMeAPI.Controllers
+{
+    public class EventController : ApiController
+    {
+    }
+}
