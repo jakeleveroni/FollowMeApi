cc30432aea30ccc7f83cc537343cef115dd9c749
Eric Garza <ejgarza@nevada.unr.edu>
2017-05-03 11:54:18 -0700

    Copy data members from UserModel to FacebookUser and change guids to strings

--- FollowMeAPI/DataModels/FacebookUser.cs
+++ FollowMeAPI/DataModels/FacebookUser.cs
@@ -25,5 +25,65 @@ namespace FollowMeAPI.DataModels
     [DynamoDBTable("Users")]
     public class FacebookUser
     {
+        // PROPERTIES AND MEMBERS
+        [DataMember(Name="FaceBookUserID")]
+        [DynamoDBHashKey("FaceBookUserID")]
+        public string UserId { get; set; }
+
+        [DataMember(Name = "ActiveTrip")]
+        [DynamoDBProperty("ActiveTrip")]
+        public string ActiveTrip { get; set; }
+
+        [DataMember(Name = "UserName")]
+        [DynamoDBProperty("UserName")]
+        public string UserName { get; set; }
+
+        [DataMember(Name = "Name")]
+        [DynamoDBProperty("Name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "Bio")]
+        [DynamoDBProperty("Bio")]
+        public string Bio { get; set; }
+
+        [DataMember(Name = "Email")]
+        [DynamoDBProperty("Email")]
+        public string Email { get; set; }
+
+        [DataMember(Name = "Password")]
+        [DynamoDBProperty("Password")]
+        public string Password { get; set; }
+
+        [DataMember(Name = "BirthDate")]
+        [DynamoDBProperty("BirthDate")]
+        public string BirthDate { get; set; }
+
+        [DataMember(Name = "NumberOfTrips")]
+        [DynamoDBProperty("NumberOfTrips")]
+        public uint NumberOfTrips { get; set; }
+
+        [DataMember(Name = "TripIds")]
+        [DynamoDBProperty("TripIds")]
+        public List<string> TripIds { get; set; }
+
+        [DataMember(Name = "TotalMilesTraveled")]
+        [DynamoDBProperty("TotalMilesTraveled")]
+        public ulong TotalMilesTraveled { get; set; }
+        
+        [DataMember(Name = "Friends")]
+        [DynamoDBProperty("Friends")]
+        public List<string> Friends { get; set; }
+
+        [DataMember(Name = "Followers")]
+        [DynamoDBProperty("Followers")]
+        public List<string> Followers { get; set; }
+
+        [DataMember(Name = "Following")]
+        [DynamoDBProperty("Following")]
+        public List<string> Following { get; set; }
+
+        [DataMember(Name="Badges")]
+        [DynamoDBProperty("Badges")]
+        public List<int> Badges { get; set; }
     }
 }
