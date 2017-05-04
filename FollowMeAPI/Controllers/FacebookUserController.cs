09b164d02ba06cb05b63b4f1f863913b2a71ba99
Eric Garza <ejgarza@nevada.unr.edu>
2017-05-03 21:38:59 -0700

    Create FacebookUser Controller

---
+++ FollowMeAPI/Controllers/FacebookUserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using FollowMeAPI.DataModels;
+using Newtonsoft.Json;
+
+namespace FollowMeAPI.Controllers
+{
+    public class FacebookUserController : ApiController
+    {
+    }
+}
